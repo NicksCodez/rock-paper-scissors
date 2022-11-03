509b83f51ef7203ae88cfc8d6cd4067cb3612818
NicksCodez <elisei.nicolae08@gmail.com>
2022-11-03 16:30:53 +0200

    Implement best of 5 rounds

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
 const choices = ["Rock", "Paper", "Scissors"];
-let playerChoice = prompt("Choose your weapon brave traveler");
+//let playerChoice = prompt("Choose your weapon brave traveler");
 
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3);
-    console.log(choice);
     return choice;
 }
 
@@ -21,4 +20,29 @@ function playGame(playerSelection, computerSelection){
     else return(`You lose! ${playerSelection} is beat by ${choices[computerSelection]}!`);
 }
 
-console.log(playGame(playerChoice, getComputerChoice()));
+//console.log(playGame(playerChoice, getComputerChoice()));
+
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for(let i = 0; i < 5; i++){
+        console.log(`Current score is:\nPlayer score: ${playerScore}\nComputer score: ${computerScore}`);
+        let game = playGame(prompt("Choose your weapon brave traveler"), getComputerChoice());
+        console.log(game);
+        if(game.includes("You win!")){
+            playerScore++;
+        }
+        else if(game.includes("You lose!")){
+            computerScore++;
+        }
+        if(playerScore === 3 || computerScore === 3) break;
+    }
+    if(playerScore - computerScore > 0){
+        return("You won!!!!");
+}
+    else if(playerScore - computerScore < 0) return("The computer won, n00b");
+    else return "This is lame, nobody wins";
+}
+
+
+console.log(game());
